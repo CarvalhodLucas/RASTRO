92809a26d2152388b5898274ac68aec7b7cf7b52
CarvalhodLucas <m.linhares@hotmail.com>
2026-04-12 16:58:15 +0200

    correcao do footer2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Link from "next/link";
-import { YouTube, Instagram, Music2 } from "lucide-react";
+import { Youtube, Instagram, Music2 } from "lucide-react";
 
 export default function Footer() {
     const currentYear = new Date().getFullYear();
@@ -28,7 +28,7 @@ export default function Footer() {
         {
             name: "YouTube",
             href: "https://www.youtube.com/@RastroIA",
-            icon: YouTube,
+            icon: Youtube,
             hoverColor: "hover:text-[#FF0000]",
         },
         {
